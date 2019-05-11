94c89efe9a5adfa3f81bf2ef4a425c86e59706e5
Алексей <catdog50rus@gmail.com>
2019-05-11 16:39:53 +0300

    Добавьте файлы проекта.

---
+++ WpfAppReserveCopy/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppReserveCopy
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProgramClose_Click(object sender, RoutedEventArgs e)
+        {
+        
+        }
+
+        private void ButtonCatalog_Click(object sender, RoutedEventArgs e)
+        {
+            //OpenFileDialog oc = new OpenFileDialog();
+            //oc.ShowDialog();
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            fb.ShowDialog();
+            string folder = fb.SelectedPath;
+            
+        }
+    }
+}
